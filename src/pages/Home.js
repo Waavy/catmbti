de3db1a349eca7ffef5b41ca89088829c731bd15
Waavy <benarr91@naver.com>
2022-06-05 17:46:48 +0900

    add route logic

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,9 +2,16 @@ import React from "react";
 import styled from "styled-components";
 import PangImage from "../assets/ggompang.jpeg";
 import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
 
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleClickButton = () => {
+    // usehistory
+    navigate('/question');
+  }
   return (
     <Wrapper>
       <Header>예비집사 판별기</Header>
@@ -12,13 +19,16 @@ const Home = () => {
         <Title>나에게 맞는 주인님은?</Title>
         <LogoImage>
           <img 
-            className="rounded-circle"
-            src={ PangImage }
-            width={350}
-            height={350} />
+            className = "rounded-circle"
+            src = { PangImage }
+            width = { 350 }
+            height = { 350 } />
         </LogoImage>
         <Desc>MBTI를 기반으로 하는 나랑 잘 맞는 고양이 찾기</Desc>
-        <Button>Test 시작하기</Button>
+        <Button 
+          style = {{fontSize: "24px"}}
+          onClick = {handleClickButton}
+        >Test 시작하기</Button>
       </Contents>
     </Wrapper>
   );
@@ -29,6 +39,7 @@ export default Home;
 const Wrapper = styled.div`
   height: 100vh;
   width: 100%;
+  font-family: "SimKyungha";
 `
 
 const Header = styled.div`
@@ -36,6 +47,7 @@ const Header = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  
 `
 
 const Contents = styled.div`
