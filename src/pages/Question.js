23115fcff9266738c0e21a9fa729fcd6efdb00bf
Waavy <benarr91@naver.com>
2022-06-05 18:45:29 +0900

    add question logic

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,37 +1,100 @@
-import React from "react";
+import { React, useState } from "react";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 import { ProgressBar, Button } from "react-bootstrap";
 import { QuestionData } from "../assets/data/questiondata";
 
 const Question = () => {
+  const [questionNo, setQuestionNo] = useState(0);
+  const [totalScore, setTotalScore] = useState([
+    { id: "EI", score: 0 },
+    { id: "SN", score: 0 },
+    { id: "TF", score: 0 },
+    { id: "JP", score: 0 },
+  ]);
+  const navigate = useNavigate();
+
+  console.log('totalScore', totalScore);
+
+  const handleClickButton = (no, type) => {
+    const newScore = totalScore.map((s) =>
+      s.id === type ? { id: s.id, score: s.score + no } : s
+    );
+    
+    setTotalScore(newScore);
+
+    console.log('newScore', newScore);
+
+
+    if(QuestionData.length !== questionNo + 1 ) {
+      // 다음 문제로 넘어가기 위해 문제수 증가
+      setQuestionNo(questionNo + 1);
+    } else {
+      // 결과페이지로 넘어가기
+      navigate("/result");
+    }
+   
+
+
+    // if ( type === "EI" ) {
+    //   // 기존 스코어에 더할 값을 계산 (기존값 + 배점)
+    //   const addScore = totalScore[0].score + no;
+    //   // 새로운 객체
+    //   const newObject = { id: "EI", score: addScore };
+    //   // splice 사용해 새로운 객체를 해당객체 자리에 넣어줌
+    //   totalScore.splice(0, 1, newObject);
+    // } else if ( type === "SN") {
+    //     const addScore = totalScore[0].score + no;
+    //     const newObject = { id: "SN", score: addScore };
+    //     totalScore.splice(1, 1, newObject);
+    // } else if ( type === "TF") {
+    //     const addScore = totalScore[0].score + no;
+    //     const newObject = { id: "TF", score: addScore };
+    //     totalScore.splice(2, 1, newObject);
+    // } else if ( type === "JP") {
+    //     const addScore = totalScore[0].score + no;
+    //     const newObject = { id: "JP", score: addScore };
+    //     totalScore.splice(3, 1, newObject);
+    // }
+  }
+  
   return (
     <Wrapper>
       <ProgressBar 
         striped
         variant="danger"
-        now={80}
+        now={ (questionNo / QuestionData.length) * 100}
         style={{ marginTop: '20px' }}
       />
-      <Title>{ QuestionData[0].title }</Title>
+      <Title>{ QuestionData[questionNo].title }</Title>
       <ButtonGroup>
         <Button
           style={{
             width:'40%',
             minHeight: '200px',
-            fontSize: '16pt'
-          }}>{ QuestionData[0].answera }
+            fontSize: '16pt',
+            border: 'none',
+            backgroundColor: 'royalblue'
+          }}
+          onClick={() => handleClickButton(1, QuestionData[questionNo].type)}
+        >{ QuestionData[questionNo].answera }
         </Button>
         <Button
           style={{
             width:'40%',
             minHeight: '200px',
             fontSize: '16pt',
-            marginLeft: '20px'
-          }}>{ QuestionData[0].answerb }
+            marginLeft: '20px',
+            border: 'none',
+            backgroundColor: 'orange'
+          }}
+          onClick={() => handleClickButton(0, QuestionData[questionNo].type)}
+        >{ QuestionData[questionNo].answerb }
         </Button>
       </ButtonGroup>
     </Wrapper>
-  )
+  );
+
 }
 
 export default Question;
