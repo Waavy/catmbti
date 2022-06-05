4f4f63915e3c3540dddee59ca0399304b5a7837d
Waavy <benarr91@naver.com>
2022-06-05 17:27:08 +0900

    add home page

---
+++ src/pages/Home.js
@@ -0,0 +1,61 @@
+import React from "react";
+import styled from "styled-components";
+import PangImage from "../assets/ggompang.jpeg";
+import Button from "react-bootstrap/Button";
+
+
+const Home = () => {
+  return (
+    <Wrapper>
+      <Header>예비집사 판별기</Header>
+      <Contents>
+        <Title>나에게 맞는 주인님은?</Title>
+        <LogoImage>
+          <img 
+            className="rounded-circle"
+            src={ PangImage }
+            width={350}
+            height={350} />
+        </LogoImage>
+        <Desc>MBTI를 기반으로 하는 나랑 잘 맞는 고양이 찾기</Desc>
+        <Button>Test 시작하기</Button>
+      </Contents>
+    </Wrapper>
+  );
+}
+
+export default Home;
+
+const Wrapper = styled.div`
+  height: 100vh;
+  width: 100%;
+`
+
+const Header = styled.div`
+  font-size: 40pt;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Contents = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
+const Title = styled.div`
+  font-size: 30pt;
+  margin-top: 40px;
+`
+
+const LogoImage = styled.div`
+  margin-top: 10px;
+`
+
+const Desc = styled.div`
+  font-size: 20pt;
+  margin-top: 30px;
+  margin-bottom: 30px;
+`
